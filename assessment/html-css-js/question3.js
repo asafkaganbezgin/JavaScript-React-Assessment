7ae125887921444c86a48d6a66a1d3f929910dad
asafkaganbezgin <akb1996@hotmail.com>
2021-03-10 20:02:10 +0300

    html-css-js-question3

--- assessment/html-css-js/question3.js
+++ assessment/html-css-js/question3.js
@@ -9,3 +9,25 @@
 // Hint: google 'debouncing'
 
 // SOLUTION:
+
+// Timer id is going to be used to clear the timer.
+var timerId;
+// Grasping the input field from the HTML.
+var inputField = document.getElementById("a_text");
+
+// Function to log the text written in the input field.
+function logInputText() {
+    console.log(inputField.value);
+}
+
+// Timer to be activated after typing in input field is stopped.
+var debounceFunction = function (func, delay) {
+    clearTimeout(timerId);
+    timerId = setTimeout(func, delay);
+};
+
+// Adding an event listener to input field and calling the timer function.
+document.getElementById("a_text").addEventListener("input", function (e) {
+    e.preventDefault();
+    debounceFunction(logInputText, 500);
+});
