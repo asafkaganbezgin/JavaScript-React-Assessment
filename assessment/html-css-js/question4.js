2f36d7e7cc25d8320efa3416b3244812894c78e1
asafkaganbezgin <akb1996@hotmail.com>
2021-03-10 21:52:32 +0300

    html-css-js-question4

--- assessment/html-css-js/question4.js
+++ assessment/html-css-js/question4.js
@@ -9,3 +9,24 @@
 // For 'Go Up and Down' button, it should go up touch the top of the viewport and return to middle in 1 second
 // Example is presented in question4.gif
 // Don't forget to add your css and javascript to index.html file
+
+// Grasping the HTML elements
+var rotate = document.getElementById("rotate-button");
+var upDown = document.getElementById("up-down-button");
+var myDiv = document.getElementById("to-be-animated");
+
+// Rotating the div when Rotate 360 button is pressed
+rotate.onclick = function () {
+    myDiv.classList.add("rotateAnimation");
+    myDiv.onanimationend = function () {
+        myDiv.classList.remove("rotateAnimation");
+    };
+};
+
+// Moving the div up and down when Go Up and Down button is clicked
+upDown.onclick = function () {
+    myDiv.classList.add("upDownAnimation");
+    myDiv.onanimationend = function () {
+        myDiv.classList.remove("upDownAnimation");
+    };
+};
