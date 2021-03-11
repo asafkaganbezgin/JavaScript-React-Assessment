ad1e88d980d3046029d2b90db794155cda32a508
asafkaganbezgin <akb1996@hotmail.com>
2021-03-11 03:38:45 +0300

    html-css-js-question2

--- assessment/html-css-js/question2.js
+++ assessment/html-css-js/question2.js
@@ -2,16 +2,39 @@
 // Fill inside of the optimizeFunction function!
 
 function doExpensiveTask(input) {
-  const result = 2 * input;
-  console.log("Doing expensive task...:", result);
-  return result;
+    const result = 2 * input;
+    console.log("Doing expensive task...:", result);
+    return result;
 }
 
 function optimizeFunction(func) {
-  // You shouldn't need to edit anywhere else
-  // Do your work inside this function
-  // SOLUTION:
+    // You shouldn't need to edit anywhere else
+    // Do your work inside this function
+    // SOLUTION:
 
+    /*********  MEMOIZATION ALGORITHM *********/
+
+    // Initializing the cache.
+    let cache = {};
+
+    return (n) => {
+        // If the input value is already memoized, read the value and skip the execution of expensive task.
+        if (n in cache) {
+            console.log("Same input, no need to calculate: " + cache[n]);
+            return cache[n];
+        /* If the input value is not memoized, execute the expensive task and insert the result in cache.
+        Furthermore, the previous memoized value is removed from cache so that if the initial input
+        occurs later in the code, expensive task is executed again. */
+        } else {
+            // Redirecting to the most inner function (doExpensiveTask or doAnotherExpensiveTask).
+            let result = func(n);
+            // Forgetting the older input
+            delete cache[Object.keys(cache)[0]];
+            // Inserting the calculated value to the cache.
+            cache[n] = result;
+            return result;
+        }
+    };
 }
 
 // anOptimizedFunc shouldn't execute the expensive task if new input is same as the previous one
@@ -24,9 +47,9 @@ anOptimizedFunc(4); // Should print: Same input, no need to calculate: 8
 anOptimizedFunc(2); // Should print: Doing expensive task...: 4
 
 function doAnotherExpensiveTask(input) {
-  const result = 10 * input;
-  console.log("Doing another expensive task...:", result);
-  return result;
+    const result = 10 * input;
+    console.log("Doing another expensive task...:", result);
+    return result;
 }
 
 // We can optimize another function, which should also behave similarly
